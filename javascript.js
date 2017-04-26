86bc3778bfb1a997cf8e714ebdbbf73c5854985d
Stefani Kaufman <neffie@Stefanis-MacBook-Pro.local>
2017-04-26 14:08:25 -0400

    playing with carousel

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      $('.carousel').carousel();
+
+      $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+      // Next slide
+     $('.carousel').carousel('next');
+     $('.carousel').carousel('next', 3); // Move next n times.
+     // Previous slide
+     $('.carousel').carousel('prev');
+     $('.carousel').carousel('prev', 4); // Move prev n times.
+     // Set to nth slide
+     $('.carousel').carousel('set', 4);
+    });
